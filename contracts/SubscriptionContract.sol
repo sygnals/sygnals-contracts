53c7c83bcad683a9a32900d63a3228481f1c0c59
supergithubo <winston.los.santos@gmail.com>
2018-01-23 20:25:54 +0800

    Write and test basic Subscription Contract

--- contracts/SubscriptionContract.sol
+++ contracts/SubscriptionContract.sol
@@ -1,26 +1,79 @@
-pragma solidity ^ 0.4 .4;
+pragma solidity ^0.4.18;
 
+import "./library/IterableMap.sol";
+
+/// @title SubscriptionContract - Allows traders(receivers) to subscribe to technical analysts(callers) calls
+/// @author Norf Lipsum - <norf@sygplatform.io>
 contract SubscriptionContract {
 
-  struct Subscription {
-    address analyst;
-    address subscriber;
-    uint calls;
+  using IterableMap for IterableMap.AddressUInt;
+
+  /*
+   *  Events
+   */
+  event Subscription(address indexed caller, address indexed receiver, uint indexed calls);
+
+  /*
+   *  Storage
+   */
+  mapping(address => IterableMap.AddressUInt) private subscriptionTable;
+
+  /*
+   *  Modifiers
+   */
+  modifier subscribed(address caller, address receiver) {
+      require(hasSubscribed(caller, receiver));
+      _;
   }
 
-  mapping(address => Subscription[]) private subscriptions;
+  /*
+   * Public functions
+   */
 
-  function subscribe(address _analyst, address _subscriber, uint _calls) public {
-    subscriptions[_analyst].push(Subscription(_analyst, _subscriber, _calls));
+   /// @dev Allows receiver subscription to a specified caller for a number of calls.
+   /// @param caller Address of the caller
+   /// @param receiver Address of the receiver
+   /// @param calls number of calls to subscribe
+  function addSubscription(address caller, address receiver, uint calls) public {
+    IterableMap.AddressUInt storage subscriptions = subscriptionTable[caller];
+    subscriptions.insert(receiver, calls);
+    Subscription(caller, receiver, calls);
   }
 
-  function countSubscriptions(address _analyst) public view returns(uint) {
-    return subscriptions[_analyst].length;
+   /// @dev Allows to return the total number of subscribers of a specified caller.
+   /// @param caller Address of the caller
+   /// @return Returns the total number of subscribers
+  function countSubscriptions(address caller) public view returns (uint) {
+    IterableMap.AddressUInt storage subscriptions = subscriptionTable[caller];
+    return subscriptions.size();
   }
 
-  function getSubscriptionAtIndex(address _analyst, uint index) public view returns(address analyst, address subscriber, uint calls) {
-    Subscription memory s = subscriptions[_analyst][index];
-    return (s.analyst, s.subscriber, s.calls);
+   /// @dev Allows to check if a receiver has subscribed to a specified caller
+   /// @param caller Address of the caller
+   /// @param receiver Address of the receiver
+   /// @return Returns whether the receiver has subscribed to caller
+  function hasSubscribed(address caller, address receiver) public view returns(bool) {
+    IterableMap.AddressUInt storage subscriptions = subscriptionTable[caller];
+    return subscriptions.contains(receiver);
   }
 
+   /// @dev Allows to return the metadata of a receiver's subscription for a specified caller.
+   /// @param caller Address of the caller
+   /// @param receiver Address of the receiver
+   /// @return Returns address of the caller, address of the receiver and the total number of calls
+  function getSubscription(address caller, address receiver) public subscribed(caller, receiver) view returns(address, address, uint) {
+    IterableMap.AddressUInt storage subscriptions = subscriptionTable[caller];
+    uint calls = subscriptions.get(receiver);
+    return(caller, receiver, calls);
+  }
+
+   /// @dev Allows to return the metadata of a subscription for a specified caller at a given index.
+   /// @param caller Address of the caller
+   /// @param idx Index of the subscription
+   /// @return Returns address of the caller, address of the receiver and the total number of calls
+  function getSubscriptionByIndex(address caller, uint idx) public view returns(address, address, uint) {
+    IterableMap.AddressUInt storage subscriptions = subscriptionTable[caller];
+    address receiver = subscriptions.getKeyByIndex(idx);
+    return getSubscription(caller, receiver);
+  }
 }
--- test/TestSubscriptionContract.sol
+++ test/TestSubscriptionContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^ 0.4 .4;
+pragma solidity ^0.4.18;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
@@ -7,24 +7,88 @@ import "../contracts/SubscriptionContract.sol";
 contract TestSubscriptionContract {
 
   SubscriptionContract subscriptionContract;
+  address caller = 0x2191eF87E392377ec08E7c08Eb105Ef5448eCED5;
+  address receiver1 = 0x0F4F2Ac550A1b4e2280d04c21cEa7EBD822934b5;
+  address receiver2 = 0x6330A553Fc93768F612722BB8c2eC78aC90B3bbc;
 
   function beforeEach() public {
-    subscriptionContract = SubscriptionContract(DeployedAddresses.SubscriptionContract());
+    subscriptionContract = new SubscriptionContract();
   }
 
-  function testSubscribe() public {
-    address analyst = 0x5AEDA56215b167893e80B4fE645BA6d5Bab767DE;
-    address subscriber1 = 0x6330A553Fc93768F612722BB8c2eC78aC90B3bbc;
-    address subscriber2 = 0x6330A553Fc93768F612722BB8c2eC78aC90B3bbc;
-    uint calls = 5;
+  function testAddSubscription() public {
+    uint actual;
 
-    subscriptionContract.subscribe(analyst, subscriber1, calls);
-    subscriptionContract.subscribe(analyst, subscriber2, calls);
+    subscriptionContract.addSubscription(caller, receiver1, uint(5));
+    subscriptionContract.addSubscription(caller, receiver2, uint(5));
 
-    uint actual = subscriptionContract.countSubscriptions(analyst);
-    uint expeted = 2;
-    Assert.equal(actual, expected, "Subscription count should be equal");
+    actual = subscriptionContract.countSubscriptions(caller);
+    Assert.equal(actual, uint(2), "Subscription count should be uint(2)");
+  }
 
+  function testCountSubscriptions() public {
+    uint actual;
+
+    actual = subscriptionContract.countSubscriptions(caller);
+    Assert.equal(actual, uint(0), "Subscription count should be zero");
+
+    subscriptionContract.addSubscription(caller, receiver1, uint(5));
+    subscriptionContract.addSubscription(caller, receiver2, uint(5));
+
+    actual = subscriptionContract.countSubscriptions(caller);
+    Assert.equal(actual, uint(2), "Subscription count should be uint(2)");
+  }
+
+  function testHasSubscribed() public {
+    bool actual;
+
+    actual = subscriptionContract.hasSubscribed(caller, receiver1);
+    Assert.isFalse(actual, "Receiver1 should not have been subscribed");
+
+    subscriptionContract.addSubscription(caller, receiver1, uint(5));
+
+    actual = subscriptionContract.hasSubscribed(caller, receiver1);
+    Assert.isTrue(actual, "Receiver1 should have been subscribed");
+
+    actual = subscriptionContract.hasSubscribed(caller, receiver2);
+    Assert.isFalse(actual, "Receiver2 should not have been subscribed");
+  }
+
+  function testGetSubscription() public {
+    address callerR;
+    address receiverR;
+    uint callsR;
+
+    (callerR, receiverR, callsR) = subscriptionContract.getSubscription(caller, receiver1);
+    Assert.isZero(callerR, "Subscription caller should be zero at not subscribed receiver");
+    Assert.isZero(receiverR, "Subscription receiver should be zero at not subscribed receiver");
+    Assert.isZero(callsR, "Subscription calls should be zero at not subscribed receiver");
+
+    subscriptionContract.addSubscription(caller, receiver1, uint(2));
+    subscriptionContract.addSubscription(caller, receiver2, uint(6));
+
+    (callerR, receiverR, callsR) = subscriptionContract.getSubscription(caller, receiver2);
+    Assert.equal(callerR, caller, "Subscription caller should be equal to caller");
+    Assert.equal(receiverR, receiver2, "Subscription receiver should be equal to receiver2");
+    Assert.equal(callsR, uint(6), "Subscription calls should be equal uint(6)");
+  }
+
+  function testGetSubscriptionByIndex() public {
+    address callerR;
+    address receiverR;
+    uint callsR;
+
+    (callerR, receiverR, callsR) = subscriptionContract.getSubscriptionByIndex(caller, 0);
+    Assert.isZero(callerR, "Subscription caller should be zero at unknown index");
+    Assert.isZero(receiverR, "Subscription receiver should be zero at unknown index");
+    Assert.isZero(callsR, "Subscription calls should be zero at unknown index");
+
+    subscriptionContract.addSubscription(caller, receiver1, uint(2));
+    subscriptionContract.addSubscription(caller, receiver2, uint(6));
+
+    (callerR, receiverR, callsR) = subscriptionContract.getSubscriptionByIndex(caller, 1);
+    Assert.equal(callerR, caller, "Subscription caller should be equal to caller");
+    Assert.equal(receiverR, receiver2, "Subscription receiver should be equal to receiver2");
+    Assert.equal(callsR, uint(6), "Subscription calls should be equal uint(6)");
   }
 
 }
