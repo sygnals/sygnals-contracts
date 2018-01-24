eb4e0e27ec367233c8b38c083cf1a511d61491f0
supergithubo <winston.los.santos@gmail.com>
2018-01-24 19:06:07 +0800

    Add code coverage

---
+++ .solcover.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 8545,
+  testrpcOptions: "--port 8545 --mnemonic 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'"
+};
--- truffle.js
+++ truffle.js
@@ -4,6 +4,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: 5777
-    }
+    },
+    coverage: {
+      host: "localhost",
+      port: 8545,
+      network_id: 5777,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
   }
 };
